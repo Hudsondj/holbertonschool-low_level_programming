b034b0ad4ae79f4640b89a9516527031862e2731
hudsondj <hudsondjm@gmail.com>
2023-09-25 16:56:54 -0700

    testfile

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* Should print the prompt. Comment is for betty style */
+/* Should print the prompt. Comment is for betty style 
+ * Description: see above line
+ */
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
