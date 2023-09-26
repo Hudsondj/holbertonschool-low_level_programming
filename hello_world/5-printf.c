b5cd854197570598e64f08576ade858208a100bc
hudsondj <hudsondjm@gmail.com>
2023-09-25 17:36:48 -0700

    added void

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,8 +5,8 @@
  *    *    * Description: 'the program's description'
  *     *      *
  *      *       * Return: Always 0 (Success)
- *       */
-int main()
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
