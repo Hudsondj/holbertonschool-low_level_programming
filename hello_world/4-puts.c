86f9fc70d65c39a46f688bdb826becf6d166f3f1
hudsondj <hudsondjm@gmail.com>
2023-09-25 17:09:53 -0700

    insight from betty style wiki

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 /**
- * Should print the prompt. Comment is for betty style
- * Description: see above line
-*/
+ *  * function_name - Short description, single line
+ *   * @parameterx: Description of parameter x
+ *   (* a blank line
+ *    * Description: Longer description of the function)?
+ *    (* section header: Section description)*
+ *     * Return: Description of the returned value
+ *      */
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
