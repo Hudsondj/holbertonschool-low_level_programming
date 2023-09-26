0c79879caf7fc814683456e40922fd23d44c9110
hudsondj <hudsondjm@gmail.com>
2023-09-25 18:26:58 -0700

    Should work. Getting smoother and easier

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  *  *  * main - Entry point
+ *   *   *   *
+ *    *    *    * Description: 'the program's description'
+ *     *     *      *
+ *      *      *       * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
