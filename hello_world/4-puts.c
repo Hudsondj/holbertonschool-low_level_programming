8a6ba913b6f22ce3f837c1e2625a7ab0985bdcd9
hudsondj <hudsondjm@gmail.com>
2023-09-25 17:02:04 -0700

    removed a space before */

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * Should print the prompt. Comment is for betty style 
  * Description: see above line
- */
+*/
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
