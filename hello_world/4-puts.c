3d850485aa2a5fe2e37ffff6a9e1c408a578822b
hudsondj <hudsondjm@gmail.com>
2023-09-25 17:13:02 -0700

    est test test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  *  * function_name - Short description, single line
- *   * @parameterx: Description of parameter x
  *   (* a blank line
  *    * Description: Longer description of the function)?
  *    (* section header: Section description)*
- *     * Return: Description of the returned value
+ *     * Return: sucess
 */
 	int main(void)
 {
