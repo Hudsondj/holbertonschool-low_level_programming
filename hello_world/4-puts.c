f813b9ed76576e64b99ded1809cc440843487a89
hudsondj <hudsondjm@gmail.com>
2023-09-25 17:11:09 -0700

    No spaces before */ even if it's from the betty wiki

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,7 +6,7 @@
  *    * Description: Longer description of the function)?
  *    (* section header: Section description)*
  *     * Return: Description of the returned value
- *      */
+*/
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
